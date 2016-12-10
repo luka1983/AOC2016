37b080a755a284cfb84f64c620078fd04b495bc8
luka1983 <luka.miljak@gmail.com>
2016-12-10 15:17:09 +0100

    7th puzzle

--- 07/07.py
+++ 07/07.py
@@ -1,10 +1,47 @@
 import re
 
-def test_segment(s):
-    return True
+def test_tls(s):
+    for i in range(0, len(s) - 3):
+        if s[i] != s[i+1] and s[i] == s[i+3] and s[i+1] == s[i+2]:
+            return True          
+    return False    
 
-def test_ip():
+def test_ip_tls(ip):
+    found = False
+    for i in range(0, len(ip)):
+        if i % 2 and test_tls(ip[i]):
+            return False
+        elif not i % 2 and test_tls(ip[i]):
+            found = True
+    return found
+
+def find_abas(s):
+    abas = []
+    for i in range(0, len(s) - 2):
+        if s[i] != s[i+1] and s[i] == s[i+2]:
+            abas.append(''.join([s[i], s[i+1], s[i+2]]))       
+    return abas
+
+def find_bab(s, aba):
+    for i in range(0, len(s) - 2):
+        if ''.join([s[i], s[i+1], s[i+2]]) == ''.join([aba[1], aba[0], aba[1]]):
+            return True
+    return False
+
+def test_ip_ssl(ip):
+    for i in range(0, len(ip) / 2 + 1):
+        abas = find_abas(ip[2*i])
+        if not len(abas):
+            continue
+        for j in range(0, (len(ip) - 2) / 2 + 1):
+            for aba in abas:
+                if find_bab(ip[2*j+1], aba):
+                    return True
+    return False
 
 ips = []
 with open('input.txt', 'r') as f:
-    ips = f.read().splitlines()
+    ips = map(lambda x: re.split('\[|\]', x), f.read().splitlines())
+
+print 'Number of TLS IPs: ' + str(len(filter(lambda x: test_ip_tls(x), ips)))
+print 'Number of SSL IPs: ' + str(len(filter(lambda x: test_ip_ssl(x), ips)))
